a95eab8cd279c880397f47357366caab334d8047
Kelvin <unit@work>
2013-02-07 18:15:38 +1100

    Included PCRLB calculation using Monte Carlo approx

--- code/main_JR.m
+++ code/main_JR.m
@@ -2,10 +2,6 @@
 % estimation
 %
 
-clear all
-close all
-clc
-
 
 N = 1000;             	% number of samples
 dT = 0.001;          	% sampling time step (global)
@@ -16,7 +12,9 @@ t = 0:dt:(N-1)*dt;
 
 % Intial true parameter values
 %
-parameters = SetParametersJR('seizure');
+% mode='alpha';
+mode='seizure';
+parameters = SetParametersJR(mode);
 parameters.dt = dt;
 A=parameters.A;
 a=parameters.a;
@@ -38,8 +36,11 @@ x0 = zeros(NStates,1);                   % initial state
 x = zeros(NStates,N); 
 x(:,1) = x0;
 
-Q = (sqrt(dt)*A*a*sigma)^2*eye(NStates);
-R = 0.01^2*eye(1);
+QnonSingular = (sqrt(dt)*A*a*sigma)^2*eye(NStates);
+Q = zeros(NStates);
+Q(4,4) = (sqrt(dt)*A*a*sigma)^2;
+
+R = 1^2*eye(1);
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 % Generate trajectory
@@ -47,16 +48,17 @@ R = 0.01^2*eye(1);
 
 % Define input
 %
-e = sqrt(dt)*A*a*sigma*randn(N,1);
+v = mvnrnd(zeros(NStates,1),Q,N)';
 
 % Euler-Maruyama integration
 %
 for n=1:N-1
-    x(:,n+1) = f(x(:,n))  + [0; 0; 0; e(n); 0; 0];
+    x(:,n+1) = f(x(:,n))  + v(:,n);
 end
 
 H = [0 0 1 0 -1 0];           % observation function
-y = H*x;
+w = mvnrnd(zeros(size(H,1),1),R,N)';
+y = H*x + w;
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 % Run EKF
@@ -65,8 +67,8 @@ y = H*x;
 % Prior distribution (defined by m0 & P0)
 %
 m0 = x0;
-P0 = F(x0)*Q*F(x0)';
-P0=Q;
+P0 = F(x0)*QnonSingular*F(x0)';
+
 
 % Apply EKF filter
 %
@@ -76,3 +78,110 @@ figure
 plot(t,x([1 3 5],:)'); hold on;
 plot(t,m([1 3 5],:)','--');
 
+
+
+%% 
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Compute the posterior Cramer-Rao bound (PCRB)
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+M = 1000;    % Number of Monte Carlo samples
+
+% Initialise variables
+%
+
+P=zeros(NStates,NStates,N);
+P(:,:,1)=P0;
+pcrb=zeros(NStates,N);
+
+% Initalise all trajectories
+%
+xk=zeros(NStates,M);
+for i=1:M
+    xk(:,i)=mvnrnd(m0,P0)';
+end
+
+% Closed form terms
+%
+Rinv = H'*inv(R)*H;
+
+
+% Compute the PCRB using a Monte Carlo approximation
+%
+for k=2:N
+    Fhat = zeros(NStates,NStates);
+    
+    v = mvnrnd(zeros(NStates,1),Q,M)';
+    parfor i=1:M
+        % Sample the next time point for the current trajectory realisation
+        %
+        xk(:,i) = f(xk(:,i)) + v(:,i);
+
+        % Compute the PCRB terms for the current trajectory realisation
+        %
+        Fhat = Fhat + F(xk(:,i));
+        
+    end
+    Fhat=Fhat./M;
+        
+    % Recursively compute the Fisher information matrix
+    %
+    P(:,:,k) = Fhat*inv(inv(P(:,:,k-1)) + Rinv)*Fhat' + Q;
+    
+    % Compute the PCRB at the current time
+    %
+    pcrb(:,k) = diag(P(:,:,k));
+end
+
+
+
+%% 
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Compute the MSE of the extended Kalman filter 
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%
+num_trials = 100;
+error = zeros(NStates,N);
+
+parfor r=1:num_trials
+    
+    % Create new trajectory realisation
+    %
+    v = mvnrnd(zeros(NStates,1),Q,N)';
+    x = zeros(NStates,N);
+    x(:,1)=mvnrnd(m0,P0)';
+    for i=2:N
+        x(:,i) = f(x(:,i-1)) +  v(:,i);
+    end
+
+    % Generate new observations 
+    %
+    w = mvnrnd(zeros(1,size(H,1)),R,N)';
+    z = H*x + w;
+
+    % Apply EKF filter
+    %
+    m = extended_kalman_filter(z,f,F,H,Q,R,m0,P0);
+
+    % Accumulate the estimation error
+    %
+    error = error + (x-m).^2;
+end
+
+% Calculate the mean squared error
+%
+mse = error ./ num_trials;
+
+%% Plot MSE and the PCRB
+%
+figure
+semilogy(t,mse,'x-')
+hold on;
+semilogy(t,pcrb,'-','LineWidth',2);
+title(mode);
+
+grid on;
+xlabel('Time (s)');
+ylabel('MSE');
+% export_fig(['../figures/nmm_pcrb_' mode '.pdf'])
