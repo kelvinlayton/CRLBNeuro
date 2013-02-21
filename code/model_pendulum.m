bb37c5ee2730705fe582653827bcc982e6fe72d6
Kelvin <unit@work>
2013-02-21 15:00:18 +1100

    Tweaked pendulum simulations to generate oscillations in the CRLB

--- code/model_pendulum.m
+++ code/model_pendulum.m
@@ -14,11 +14,11 @@ function [out] = model_pendulum(x, delta, mode)
     if mode(1)=='t';
         % Nonlinear transition model
         out = [x(1) + x(2)*delta; ...
-                x(2) - 10*sin(x(1))*delta];
+                x(2) - 100*sin(x(1))*delta];
 
     else
         % Jacobian
         out = [1, delta; ...
-                -10*cos(x(1))*delta, 1];
+                -100*cos(x(1))*delta, 1];
     end
 end
