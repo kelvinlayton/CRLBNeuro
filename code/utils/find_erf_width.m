72e4ce9dff7dc46e3d72b095e18e914c6c76c249
Dean Freestone <deanfreestone@gmail.com>
2013-06-13 10:29:32 +1000

    minor change

--- code/utils/find_erf_width.m
+++ code/utils/find_erf_width.m
@@ -9,6 +9,10 @@ sig1 = 1 ./ (1 + exp(rSig*(v0Sig - x)));
 rErf = lsqnonlin(@cost_func,5);
 
 
+figure
+plot(x,sig1)
+hold
+plot(x,0.5 .* ( 1 + erf((x - v0Erf)./(sqrt(2)*rErf))),'r')
 
 function diff = cost_func(r)
     sig2 = 0.5 .* ( 1 + erf((x - v0Erf)./(sqrt(2)*r)) );
