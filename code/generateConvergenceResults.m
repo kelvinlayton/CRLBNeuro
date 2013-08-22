ad8e85b7a9b7c24dbdd2cfdeb2011d9db2978564
Kelvin <unit@laptop>
2013-08-22 16:53:05 +1000

    Manuscript and MATLAB code

---
+++ code/generateConvergenceResults.m
@@ -0,0 +1,105 @@
+% This code repricates the structure of the Voss / Schiff FN simulation /
+% estimation
+clc
+close all
+clear
+
+N = 500;             	% number of samples
+dT = 0.001;          	% sampling time step (global)
+dt = 1*dT;            	% integration time step
+nn = fix(dT/dt);      	% (used in for loop for forward modelling) the integration time step can be small that the sampling (fix round towards zero)
+
+t = 0:dt:(N-1)*dt;
+
+
+% Initialise random number generator for repeatability
+rng(0);
+
+M = 100;    % Mumber of Monte Carlo trials for MSE calculation
+
+
+NStatesModels = [6 6];  
+
+% Model parameters
+params{1} = SetParametersLopes('alpha');
+params{2} = SetParametersLopes('alpha');
+
+
+% Transition functions
+models{1} = @model_Lopes;
+models{2} = @model_Lopes;
+
+% Measurement fnctions
+Hmodels{1} = [1 0 -1 0 0 0];           % Lopes
+Hmodels{2} = [1 0 -1 0 0 0];           % Lopes
+
+% Indices to plot (only potentials, not derivatives)
+displayStates{1} = [1 3 5];
+displayStates{2} = [1 3 5];
+
+noiseInd = [2 2 ];     % NEED TO CHECK THESE! (and adjust Wendling noise)
+noiseScale = [1 1 ];
+
+mseModels = cell(length(models),1);
+crbModels = cell(length(models),1);
+
+for iModel = 1:length(models)
+    
+parameters = params{iModel};
+parameters.dt = dt;
+A = parameters.A;
+a = parameters.a;
+sigma = parameters.sigma;
+
+% Transition model
+NStates = NStatesModels(iModel);                           
+f = @(x)models{iModel}(x,'transition',parameters);
+F = @(x)models{iModel}(x,'jacobian',parameters);
+
+H = Hmodels{iModel};
+
+displayInd = displayStates{iModel};
+
+% Initialise trajectory state
+x0 = zeros(NStates,1);                   % initial state
+
+% Define input
+Q = zeros(NStates);
+Q(noiseInd(iModel),noiseInd(iModel)) = noiseScale(iModel)*(sqrt(dt)*A*a*sigma)^2;
+
+% Prior distribution (defined by m0 & P0)
+%
+m0 = x0;
+if iModel==1
+%     P0 = 100.^2*eye(NStates);
+    P0 = 50.^2*diag([1 0 1 0 1 0]) + 50^2*diag([0 1 0 1 0 1]);
+else
+%     P0 = 200.^2*eye(NStates);
+    P0 = 5000.^2*diag([1 0 1 0 1 0]) + 5000^2*diag([0 1 0 1 0 1]);
+end
+
+
+Hfun = @(x)H;
+R = 10^2*eye(1);
+    
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Compute CRLB
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+crbAll = compute_pcrb_P(t,f,F,Hfun,Q,R,m0,P0,2000);
+
+crb = crbAll(:,end);
+
+
+semilogy(t,crbAll(displayInd,:))
+iModel
+hold on;
+
+crbModels{iModel} = crb(displayInd);
+
+end
+
+return;
+
+save crb_convergence_results.mat crbModels
+
+plotConvergenceResults
---
+++ code/plotConvergenceResults.m
@@ -0,0 +1,46 @@
+
+
+FS = 8;             % fontsize - point
+FS_Panel_Label = 12;
+
+font = 'arial';
+fig_width = 17;  % cm
+fig_height = 7;  % cm
+
+LW = 0.5;         % linewidth
+
+fig_filename = '/Users/kelvin/Dropbox/CRLBNeuro/manuscript/figures/pdf/AlphaSeizureBar.pdf';
+figure('units','centimeters','papersize',[fig_height fig_width],'filename',fig_filename,'position',[2 2 fig_width fig_height]);
+
+load crb_convergence_results.mat
+
+%%
+crbBar = [];
+for iModel=[1 3 2 4]
+    crbBar = [crbBar; sum(sqrt(crbModels{iModel})); nan];
+end
+
+b1=bar(crbBar(:)','BaseValue',1e-6); hold on;
+% set(gca,'YScale','log','YGrid','on')
+set(gca,'YScale','linear','YGrid','on')
+% xlim([0 20])
+
+ch1 = get(b1,'children');
+% cind = [1;1;1;1;2;2;2;2;2;2;3;3;3;3;4;4;4;4;4;4];
+cind = [1 1 2 2 3 3 4 4]';
+set(ch1,'FaceVertexCData',cind)
+cmap = [0.7556 0.8196 0.4769; 0.6590 0.4332 0.2696; 0.2245 0.3898 0.5888; 0.1831 0.9336 0.5528];
+% cmap = rand(4,3);
+colormap(cmap);
+
+laby=ylabel('RMSE (mV)');
+% set(gca,'XTick',[2 6 11 16],'XTickLabel',{'JR alpha','JR seizure','Wendling alpha','Wendling seizure'})
+set(gca,'XTick',[1 3 5 7],'XTickLabel',{'JR alpha','JR seizure','Wendling alpha','Wendling seizure'})
+% ylim([1e-6 1e3])
+ylim([0 10])
+
+set([gca laby],'fontsize',FS,'fontname',font)
+
+return
+%%
+export_fig(fig_filename)
